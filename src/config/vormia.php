20d48ea8636e6121f679499cc70ebf603c771975
Josh L. Minga <joshlminga@gmail.com>
2025-06-30 19:52:54 +0300

    fixing an error "config/vormia.php): Failed to open stream: No such file or directory

---
+++ src/config/vormia.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Third Party Services - VORMIA
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the credentials for vormia utilities.
+    |
+    */
+
+    'table_prefix' => env('VORMIA_TABLE_PREFIX', 'vrm_'),
+
+];
