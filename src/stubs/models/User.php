c25a0b2b7e868474297875d3ded511254b92939b
Josh L. Minga <joshlminga@gmail.com>
2025-07-25 22:14:54 +0300

    Vormia User Model codebase updated

--- src/stubs/models/User.php
+++ src/stubs/models/User.php
@@ -6,9 +6,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-// use Laravel\Sanctum\HasApiTokens;
 use App\Traits\Vrm\Model\HasUserMeta;
 use App\Traits\Vrm\Model\HasSlugs;
+// use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Support\Str;
 
 class User extends Authenticatable
