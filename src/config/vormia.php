938c016a53f44dfaea8da2645092ebf0b5a12cd5
Josh L. Minga <joshlminga@gmail.com>
2025-07-25 21:50:44 +0300

    Env variables updated

--- src/config/vormia.php
+++ src/config/vormia.php
@@ -22,8 +22,25 @@ return [
     |
     */
 
-    'auto_update_slugs' => env('AUTO_UPDATE_SLUGS', false),
-    'slug_approval_required' => env('SLUG_APPROVAL_REQUIRED', true),
-    'slug_history_enabled' => env('SLUG_HISTORY_ENABLED', true),
+    'auto_update_slugs' => env('VORMIA_AUTO_UPDATE_SLUGS', false),
+    'slug_approval_required' => env('VORMIA_SLUG_APPROVAL_REQUIRED', true),
+    'slug_history_enabled' => env('VORMIA_SLUG_HISTORY_ENABLED', true),
+
+    /*
+    |--------------------------------------------------------------------------
+    | MediaForge Settings
+    |--------------------------------------------------------------------------
+    |
+    | Configure MediaForge image processing driver and default settings.
+    |
+    */
+
+    'mediaforge' => [
+        'driver' => env('VORMIA_MEDIAFORGE_DRIVER', 'auto'), // 'auto', 'imagick', 'gd'
+        'default_quality' => env('VORMIA_MEDIAFORGE_DEFAULT_QUALITY', 85),
+        'default_format' => env('VORMIA_MEDIAFORGE_DEFAULT_FORMAT', 'webp'),
+        'auto_override' => env('VORMIA_MEDIAFORGE_AUTO_OVERRIDE', false),
+        'preserve_originals' => env('VORMIA_MEDIAFORGE_PRESERVE_ORIGINALS', true),
+    ],
 
 ];
